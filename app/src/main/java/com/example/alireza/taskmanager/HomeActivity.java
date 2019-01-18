31c6094769f4a05c326d49baea9916ed0eefc84c
MaTT <matiii.rahat@yahoo.com>
2019-01-18 16:27:19 +0330

    Users added

--- app/src/main/java/com/example/alireza/taskmanager/HomeActivity.java
+++ app/src/main/java/com/example/alireza/taskmanager/HomeActivity.java
@@ -3,7 +3,12 @@ package com.example.alireza.taskmanager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class HomeActivity extends AppCompatActivity {
+
+    ArrayList<User> users = new ArrayList<>();
+    ArrayList<Task> tasks = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
