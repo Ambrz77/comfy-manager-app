dc63fb0d63ca922ab967f63c4adc659755b99261
Alireza <a.mahdibarzi@hotmail.com>
2019-01-18 15:41:22 +0330

    Test Commit

--- app/src/main/java/com/example/alireza/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/alireza/taskmanager/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    static int x=0;
+    static int x = 0;
+    String a = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
