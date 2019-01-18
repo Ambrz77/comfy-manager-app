ca04b5b09a805366b13d652197157f0146192c00
MaTT <matiii.rahat@yahoo.com>
2019-01-18 15:35:51 +0330

    Task Completed

---
+++ app/src/main/java/com/example/alireza/taskmanager/Task.java
@@ -0,0 +1,78 @@
+package com.example.alireza.taskmanager;
+
+public class Task {
+    String subject;
+    int year=0,month=0,day=0,hour=0,min=0;//time
+    Priority priority;
+    String detail;
+
+    public Task(String subject, Priority priority) {
+        this.subject = subject;
+        this.priority = priority;
+    }
+
+    public void setTime(int year, int month, int day, int hour, int min) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        this.hour = hour;
+        this.min = min;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int compareTime(Task task) {
+        if (year > task.year) {
+            return 1;
+        }
+        if(year < task.year) {
+            return -1;
+        }
+        if (month > task.month) {
+            return 1;
+        }
+        if (month < task.month) {
+            return -1;
+        }
+        if (day > task.day) {
+            return 1;
+        }
+        if (day < task.day) {
+            return -1;
+        }
+        if (hour > task.hour) {
+            return 1;
+        }
+        if (hour < task.hour) {
+            return -1;
+        }
+        if (min > task.min) {
+            return 1;
+        }
+        if (min < task.min) {
+            return -1;
+        }
+
+        return 0;
+    }
+}
+
+class Priority {
+    String name;
+    private int importance;
+
+    public Priority(String name, int importance) {
+        this.name = name;
+        this.importance = importance;
+    }
+
+    public int compare(Priority p) {
+        if (importance < p.importance) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
