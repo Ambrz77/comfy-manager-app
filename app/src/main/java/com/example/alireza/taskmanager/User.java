03ae87e156ee7d93bb5e3304fea99e7812ba9249
MaTT <matiii.rahat@yahoo.com>
2019-01-19 00:53:43 +0330

    bugs fixed!

---
+++ app/src/main/java/com/example/alireza/taskmanager/User.java
@@ -0,0 +1,42 @@
+package com.example.alireza.taskmanager;
+
+import java.util.ArrayList;
+
+public class User {
+    private String Username,Email,Password;
+    String Name,Familyname;
+    int Type;//Normal/Silver/Gold user
+    boolean loggedIn = false;
+    ArrayList<Task> tasks = new ArrayList<>();
+    ArrayList<Priority> priorities = new ArrayList<>();
+
+    public String getUsername() {
+        return Username;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public User(String username, String email, String name, String password, int type) {
+        Username = username;
+        Email = email;
+        Name = name;
+        Password = password;
+        Type = type;
+    }
+
+    public User(String username, String email, String password, String name, String familyname, int type) {
+        Username = username;
+        Email = email;
+        Password = password;
+        Name = name;
+        Familyname = familyname;
+        Type = type;
+    }
+
+}
