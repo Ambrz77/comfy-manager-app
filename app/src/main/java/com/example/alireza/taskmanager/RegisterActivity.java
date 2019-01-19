2bbc2f2c7ebea3b7a8c669650d004c97aff61fa3
MaTT <matiii.rahat@yahoo.com>
2019-01-19 16:08:43 +0330

    Register Almost Complete!

--- app/src/main/java/com/example/alireza/taskmanager/RegisterActivity.java
+++ app/src/main/java/com/example/alireza/taskmanager/RegisterActivity.java
@@ -1,19 +1,53 @@
 package com.example.alireza.taskmanager;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Server server=new Server();
     String user,email,name,family,pass;
     int type=0;
+    EditText u,e,n,f,p;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button signup = (Button) findViewById(R.id.signup);
+        u = findViewById(R.id.userText);
+        e = findViewById(R.id.emailText);
+        n = findViewById(R.id.nameText);
+        f = findViewById(R.id.familyText);
+        p = findViewById(R.id.passText);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user = u.getText().toString();
+                email = e.getText().toString();
+                name = n.getText().toString();
+                family = f.getText().toString();
+                pass = p.getText().toString();
+                try {
+                    if (!(server.isUser(user) == 1 || server.isUser(email) == 2)) {
+                        server.Register(user, email, name, pass, family, type);
+                        System.out.println(u + " Registered!");
+                        Intent Hintent = new Intent(RegisterActivity.this, HomeActivity.class);
+                    } else {
+                        throw new ExistedUserException(u + " is already registered!");
+                    }
+                } catch (ExistedUserException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
     }
 
     public void onRadioButtonClicked(View view) {
