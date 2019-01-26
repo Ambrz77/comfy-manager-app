8abd4d262d491cab440fd5d40921c00909d01f58
MaTT <matiii.rahat@yahoo.com>
2019-01-26 05:14:06 +0330

    add priority complete

--- app/src/main/java/com/example/alireza/taskmanager/NewPriority.java
+++ app/src/main/java/com/example/alireza/taskmanager/NewPriority.java
@@ -3,6 +3,7 @@ package com.example.alireza.taskmanager;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,39 +11,47 @@ import android.widget.Toast;
 public class NewPriority extends AppCompatActivity {
     SeekBar seekBar;
     TextView textView;
+    String user;
+    EditText pr;
+    int progress_value;
+    Server server = new Server();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_priority);
-        SBar();
+        pr = findViewById(R.id.pr);
+        Bundle extras = getIntent().getExtras();
+        if(extras != null)
+            user = extras.getString("user");
+        {
+            seekBar = (SeekBar)findViewById(R.id.seekBar);
+            textView = (TextView)findViewById(R.id.MyMail);
+            textView.setText(seekBar.getProgress() + " / " + seekBar.getMax());
+
+
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    progress_value=progress;
+                    textView.setText(progress + " / " + seekBar.getMax());
+                    Toast.makeText(NewPriority.this, "SeekBar in progress", Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    Toast.makeText(NewPriority.this, "Start Tracking", Toast.LENGTH_LONG).show();
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    textView.setText(progress_value + " / " + seekBar.getMax());
+                    Toast.makeText(NewPriority.this, "Stoped Tracking", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+        server.getUser(user).priorities.add(new Priority(pr.getText().toString(), progress_value));
     }
-    public void SBar(){
-        seekBar = (SeekBar)findViewById(R.id.seekBar);
-        textView = (TextView)findViewById(R.id.MyMail);
-        textView.setText(seekBar.getProgress() + " / " + seekBar.getMax());
 
-
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progress_value;
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                progress_value=progress;
-                textView.setText(progress + " / " + seekBar.getMax());
-                Toast.makeText(NewPriority.this, "SeekBar in progress", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(NewPriority.this, "Start Tracking", Toast.LENGTH_LONG).show();
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                textView.setText(progress_value + " / " + seekBar.getMax());
-                Toast.makeText(NewPriority.this, "Stoped Tracking", Toast.LENGTH_LONG).show();
-            }
-        });
-    }
 }
